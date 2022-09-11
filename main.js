00b105f88bba5915622d7a21e94beb96473ad405
contrast-zone <33665826+contrast-zone@users.noreply.github.com>
2022-09-11 13:06:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ var fs = require('fs');
 
 var files = require ("./src/file-funcs.js");
 
+var mimeTypes = {
+      "html": "text/html",
+      "jpeg": "image/jpeg",
+      "jpg": "image/jpeg",
+      "png": "image/png",
+      "svg": "image/svg+xml",
+      "json": "application/json",
+      "js": "text/javascript",
+      "css": "text/css"
+    };
+
 http.createServer(async function (req, res) {
     var q = url.parse(req.url, true);
     var filename = "." + q.pathname;
@@ -41,8 +52,22 @@ http.createServer(async function (req, res) {
                 res.writeHead(404, {'Content-Type': 'text/html'});
                 return res.end("404 Not Found: " + filename);
                 console.log(filename);
-            } 
-            res.writeHead(200, {'Content-Type': 'text/html'});
+            }
+            /*
+            if (filename.substring (filename.length - 3, filename.length) === "svg") {
+                res.writeHead(200, {'Content-Type': mime.contentType(filename)});
+                console.debug (mime.contentType(filename));
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+            }
+            */
+            var mimeType = mimeTypes[filename.split('.').pop()];
+          
+            if (!mimeType) {
+                mimeType = 'text/plain';
+            }
+          
+            res.writeHead(200, { "Content-Type": mimeType });
             res.write(data);
             return res.end();
         });
