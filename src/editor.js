be48184c1c8919dc63b6f027628f9b7c02353773
contrast-zone <33665826+contrast-zone@users.noreply.github.com>
2022-09-27 11:20:22 +0200

    Add files via upload

--- src/editor.js
+++ src/editor.js
@@ -5,10 +5,34 @@ var editor = (function (obj) {
     }
 }) (
     (function () {
+        function tag (str) {
+            return `((?<=\\((\\s|·)*)${str}(?=(\\s|·)))`
+        }
+
+        var opt = {
+            font: "1em monospace",
+            tabWidth: 4,
+            colorText: "rgb(208,208,208)",
+            colorTextBack: "transparent",
+            colorSelection: "white",
+            colorSelectionBack: "gray",
+            colorKeyword: "rgb(128,128,128)",
+            colorKeywordBack: "transparent",
+            colorStringAndComment: "rgb(128,128,128)",
+            colorStringAndCommentBack: "transparent",
+            colorBracketMatch: "white",
+            colorBracketMatchBack: "rgb(75,75,75)",
+            keywords: [`${tag('hruler')}|${tag('bold')}|${tag('italic')}|${tag('clist')}|${tag('eitem')}|${tag('xitem')}|${tag('yitem')}|${tag('comment')}|${tag('tree')}|${tag('node')}|${tag('name')}|${tag('branches')}|${tag('title')}|${tag('heading1')}|${tag('heading2')}|${tag('heading3')}|${tag('heading4')}|${tag('heading5')}|${tag('heading6')}|${tag('paragraph')}|${tag('hyperlink')}|${tag('address')}|${tag('target')}|${tag('bcode')}|${tag('icode')}|${tag('bquote')}|${tag('olist')}|${tag('ulist')}|${tag('litem')}|${tag('image')}|${tag('source')}`],
+            stringsAndComments: "(\"([^\"\\\\\\n]|(\\\\.))*((\")|(\\n)|($)))",
+            hilightMatchingBraces: false,
+            spaceDots: true
+        }
+
         edit = function (node, options) {
             "use strict";
             if (!options)
-                options = {
+                options = opt;
+                /*{
                     font: "8pt monospace",
                     tabWidth: 4,
                     colorText: "rgb(208,208,208)",
@@ -26,6 +50,7 @@ var editor = (function (obj) {
                     hilightMatchingBraces: true,
                     spaceDots: false
                 }
+                */
 
             var ww, hh;
             var rndid = Math.round (Math.random () * 32768);
@@ -609,6 +634,9 @@ var editor = (function (obj) {
         
         hilight = function (text, options) {
             "use strict";
+
+            if (!options)
+                options = opt;
 
             function hilightAll() {
                 text = hilight (text);
